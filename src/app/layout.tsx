13915e6e48051c39cae5f03b2b7f7f7e34220698
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2025-06-29 12:25:39 +0600

    image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,11 +51,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/favicon.svg",
+        url: "/blackshadow.svg",
         sizes: "any",
       }
     ],
-    shortcut: "/favicon.svg?v=2",
+    shortcut: "/blackshadow.svg?v=2",
     apple: "/apple-touch-icon.svg?v=2",
   },
 };
@@ -69,7 +69,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <link rel="icon" href="/favicon.svg" sizes="any" />
+        <link rel="icon" href="/blackshadow.svg" sizes="any" />
       </head>
       <body
         className={cn(
